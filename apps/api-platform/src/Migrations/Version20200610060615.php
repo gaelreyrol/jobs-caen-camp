797e94905c423c17b107b743759997fc45cb7ac9
Gaël Reyrol <me@gaelreyrol.dev>
2020-06-12 14:57:02 +0200

    feat: add base api-platform

---
+++ apps/api-platform/src/Migrations/Version20200610060615.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200610060615 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Add UUIDv4 extension';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->addSql('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->addSql('DROP EXTENSION IF EXISTS "uuid-ossp"');
+    }
+}
